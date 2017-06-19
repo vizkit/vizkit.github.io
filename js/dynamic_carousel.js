afbff00fd67b7723c0bf37cce2567f3549b22f3d
annariasdu <annariasdu@hotmail.com>
2017-06-19 15:14:04 +0200

    Update dynamic_carousel.js

--- js/dynamic_carousel.js
+++ js/dynamic_carousel.js
@@ -95,7 +95,7 @@ function demoFromHTML() {
   
       //    pdf = new jsPDF('p', 'pt', 'a4', false);
           pdf = new jsPDF();
-          pdf.text(30,30,"hello");
+          pdf.text("hello", 30,30);
        //  logo.src = "1.png";
         //    pdf.addImage(logo, 'JPEG', 2, 2, 590,850);
   
