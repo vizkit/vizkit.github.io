1872baab8c6380176bfb94d9640d323a9305c7d1
annariasdu <annariasdu@hotmail.com>
2017-06-20 15:26:56 +0200

    Update dynamic_carousel.js

--- js/dynamic_carousel.js
+++ js/dynamic_carousel.js
@@ -165,7 +165,7 @@ groupe = 1;
           if(localStorage.duree == "longue_adult"){
             if(localStorage.dataset == "un"){
 
-             alert("canvicanvi");
+            
 
               var logo = new Image();   
       //       for (groupe = 1; groupe < 5; groupe++) {          
